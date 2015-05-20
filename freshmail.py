ae2f80d685ab6f89a6d8a2bf12e229758c38009d
ChrisHacker <chris@myshorttrack.com>
2015-05-20 00:26:19 -0400

    Adds getSubscriber, deleteSubscriber, findSubscriber, and findSubscriberInLists methods

--- freshmail.py
+++ freshmail.py
@@ -2,6 +2,15 @@ import requests
 import json
 import hashlib
 
+"""
+subscriber status codes for use with API: (undocumented in API)
+1 active
+2 activation pending
+3 not activated
+4 resigned
+5 soft bouncing
+8 hard bouncing
+"""
 
 class FreshMail(object):
     """
@@ -46,7 +55,7 @@ class FreshMail(object):
     def getHttpCode(self):
         return self.httpCode
 
-    def request(self, url, payload=None, raw_resonse=False):
+    def request(self, url, payload=None, raw_response=False, method='POST'):
         """
         Makes request to REST API. Add payload data for POST request.
         :param url: API endpoint
@@ -71,7 +80,12 @@ class FreshMail(object):
             'X-Rest-ApiKey': self.api_key,
             'X-Rest-ApiSign': m.hexdigest()
         }
-        r = requests.post(full_url, data=post_data, headers=headers)
+        if method is 'POST':
+          r = requests.post(full_url, data=post_data, headers=headers)
+        elif method is 'GET':
+          r = requests.get(full_url, data=post_data, headers=headers)
+        else:
+          raise FreshMailException({'message':'GET or POST required methods. Got {}'.format(method)})
 
         self.httpCode = r.status_code
 
@@ -84,13 +98,13 @@ class FreshMail(object):
         self.response = r.json()
         self.rawResponse = r.content
 
-        if raw_resonse:
+        if raw_response:
             return self.rawResponse
         else:
             return self.response
 
     def addSubscriber(self, email, list_hash, state=3, confirm=1, custom_fields=None):
-        #self, url, payload=None, raw_resonse=False
+        #self, url, payload=None, raw_response=False
         payload = {
           'email' : email,
           'list' : list_hash,
@@ -101,8 +115,72 @@ class FreshMail(object):
         response = self.request(url, payload)
         print response
         return
+        
+    def deleteSubscriber(self, email, list_hash, state=3, confirm=1, custom_fields=None):
+        #self, url, payload=None, raw_response=False
+        payload = {
+          'email' : email,
+          'list' : list_hash
+        }
+        url = 'subscriber/delete'
+        response = self.request(url, payload)
+        print response
+        return
 
+    def getLists(self):
+        #self, url, payload=None, raw_response=False
+        url = 'subscribers_list/lists'
+        response = self.request(url)
+        if response.get('status') and response.get('status') == 'OK':
+          return response.get('lists')
+        else:
+          return None
 
+    def getSubscriber(self, email, list_hash):
+        #self, url, payload=None, raw_response=False
+        url = 'subscriber/get/{}/{}'.format(list_hash, email)
+        response = self.request(url,method='GET')
+        return response
+
+    def findSubscriber(self, email):
+        #slow with large number of lists
+        lists = self.getLists()
+        if len(lists) > 0:
+          subscribed_lists = []
+          for list in lists:
+            list_hash = list['subscriberListHash']
+            try:
+              result = self.getSubscriber(email, list_hash)
+              subscribed_list = { 'list_hash' : list_hash, 'name': list['name'], 'subscriber':result}
+              subscribed_lists.append(subscribed_list)
+            except:
+              #ignore not found errors
+              pass
+          
+          return subscribed_lists
+              
+        else:
+          raise FreshMailException({'message':'No lists found'})
+
+    def findSubscriberInLists(self, email, lists):
+        #slow with large number of lists
+        if len(lists) > 0:
+          subscribed_lists = []
+          for list in lists:
+            list_hash = list['subscriberListHash']
+            try:
+              result = self.getSubscriber(email, list_hash)
+              subscribed_list = { 'list_hash' : list_hash, 'name': list['name'], 'subscriber':result}
+              subscribed_lists.append(subscribed_list)
+            except:
+              #ignore not found errors
+              pass
+          
+          return subscribed_lists
+              
+        else:
+          raise FreshMailException({'message':'No lists found'})
+        
 
 class FreshMailException(Exception):
     pass
